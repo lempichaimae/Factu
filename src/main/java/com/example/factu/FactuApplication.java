4dd6845b63272c19b7bd56ffe009c13d7972d9cb
lempichaimae <chaimae.khamma@gmail.com>
2024-02-02 23:20:17 +0100

    first commit

---
+++ src/main/java/com/example/factu/FactuApplication.java
@@ -0,0 +1,13 @@
+package com.example.factu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/factu/FactuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.factu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
